3fcbe484ff2a2b3acdaad2ed97d7998948abd76d
LunarStorm97 <ian8388382@proton.me>
2025-01-03 04:21:03 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,7 @@
 
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_a23xq.mk
 
-COMMON_LUNCH_CHOICES := twrp_a23xq-eng
+COMMON_LUNCH_CHOICES := \
+    twrp_a23xq-user \
+    twrp_a23xq-userdebug \
+    twrp_a23xq-eng
